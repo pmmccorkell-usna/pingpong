c1bbd51620caddf193a8638ff937a22223a8fd05
pmmccorkell-usna <78446270+pmmccorkell-usna@users.noreply.github.com>
2022-09-30 08:53:19 -0400

    asdf

--- code.py
+++ code.py
@@ -4,18 +4,13 @@
 # Robotics and Control TSD
 #
 
-# What context is code.py being executed from?
-# 	default startup is '__main__'
-#	REPL is 'code'
-#	If imported from another script, will display that higher script.
 print("Hello " + __name__)
-
 from pingpong import Pingpong	# Class for the lab environment
 import atexit		# For error handling.
+from controller import *
 
 # Class for the lab environment.
 lab = Pingpong()
-
 
 ###################################
 ######## Reset and Program ########
@@ -35,108 +30,16 @@ def exit_program(reason='None'):
 	print('exit_program called from ' + str(reason))
 
 	# Turn the mosfet off before deinit all the objects.
-	lab.set_pwm(0)
+	for _ in range(3):
+		lab.set_pwm(0)
 
 	# Deinit all the pin assignments from each Class.
 	lab.deinit()
 
-	# If Matlab interface is implemented, inform Matlab that the program quit.
-	# print('LOG: exiting program')
 
 # Causes the registered function() to be called if the program fails for any reason,
 #	or the program naturally comes to a logical conclusion.
 atexit.register(exit_program)
-
-###################################
-########## Notes to Self ##########
-###################################
-###################################
-
-# sensor		21888	at bottom
-
-# pwm		0.266	when it can maintain a current (~ 50 mA)
-
-
-
-###################################
-####### Example Controllers #######
-###################################
-###################################
-
-# error = 0
-# last_error = 0
-# i_term = 0
-# # @tictoc
-# def control_loop_P():
-# 	global pid, target_speed, error, last_error
-# 	quanser_305.encoder_loop()
-# 	error = target_speed - (quanser_305.get_dx() / quanser_305.get_dt())
-# 	p_term = pid['Kp'] * error
-# 	last_error = error
-# 	# print('p: '+str(p_term)+', i: '+str(0)+', d: '+str(0)+', err: '+str(error))
-# 	quanser_305.digipot.set_pot(bias + p_term)
-
-# # @tictoc
-# def control_loop_PI():
-# 	global pid, target_speed, error, last_error, i_term
-# 	quanser_305.encoder_loop()
-# 	dt = quanser_305.get_dt()
-# 	error = target_speed - (quanser_305.get_dx() / dt)
-# 	p_term = pid['Kp'] * error
-# 	i_term += (pid['Ki'] * error * dt)
-# 	last_error = error
-# 	# print('p: '+str(p_term)+', i: '+str(i_term)+', d: '+str(0)+', err: '+str(error))
-# 	quanser_305.digipot.set_pot(bias + p_term + i_term)
-
-# # @tictoc
-# def control_loop_PID():
-# 	global pid, target_speed, error, last_error, i_term
-# 	quanser_305.encoder_loop()
-# 	dt = quanser_305.get_dt()
-# 	error = target_speed - (quanser_305.get_dx() / dt)
-# 	p_term = pid['Kp'] * error
-# 	i_term += pid['Ki'] * error * dt
-# 	d_term = pid['Kd'] * (error - last_error) / dt
-# 	# print('p: '+str(p_term)+', i: '+str(i_term)+', d: '+str(d_term)+', err: '+str(error))
-# 	quanser_305.digipot.set_pot((bias + p_term + i_term + d_term))
-# 	last_error = error
-
-
-
-###################################
-##### Control the Controllers #####
-###################################
-###################################
-
-# controller_func_name = control_loop_PID
-# time_limit = 1
-# controller_rate = 0.00005
-# target_pos = (10 / tau) * encoder_counts_per_rev / 10**9		# 10 rad/s
-# bias = 20/511
-# sample_offset = 2	# Change how many samples away to get dx and dt.
-# 					# ie,	last sample is 100th pair of position and time.
-# 					# 		If set to 1, compares that to the 99th entry.
-# 					#		To compare against 98th entry, set this to 2.
-# pid = {
-# 	'Kp' : 0.00003 * 10**9,
-# 	'Ki' : 0.0004,
-# 	'Kd' : 0.00000001  * 10**18
-# }
-
-
-# def runit():
-# 	# quanser_305.auto_control()
-# 	global controller_rate, time_limit, controller_func_name, target_speed, sample_offset
-# 	quanser_305.change_sample_offset(sample_offset)
-# 	quanser_305.attach_controller(controller_func_name, controller_rate, time_limit)
-# 	quanser_305.run_controller()
-# 	print("LOG: final err: %0.2f%%" %(100 * last_error / target_speed))
-# 	sleep(1)
-
-
-
-
-
 
 ###################################
 ######## Startup Section ##########
@@ -145,9 +48,16 @@ atexit.register(exit_program)
 
 if __name__ == '__main__':
 	print("running from main")
-	lab.tsd_profile_characteristics()
+	while(1):
+		controller(lab,v_target=1)
+		sleep(2)
+		controller(lab,v_target=2)
+		sleep(2)
+		lab.play_random_music()
+	# lab.tsd_profile_characteristics()
 	# lab.play_random_music(-1,600,0.5)
 
 
 if __name__ == 'code':
-	lab.tsd_profile_characteristics()
+	lab.play_random_music(-1,600,0.5)
+	# lab.tsd_profile_characteristics()
---
+++ controller.py
@@ -0,0 +1,111 @@
+from time import sleep, monotonic_ns
+
+#
+# CHANGE THIS TO SELECT P, PI, or PID
+#
+active_loop_func = 'p'
+
+
+###################################
+########## PID FUNCTIONS ##########
+###################################
+###################################
+
+i_term = 0
+last_error = 0
+
+def p(dt,error):
+	global i_term,last_error
+	bias = 0.35
+	kp = 0.9
+
+	p_term = kp * error
+	i_term += 0
+	d_term = 0
+
+	return p_term + i_term + d_term + bias
+
+
+def pi(dt,error):
+	global i_term,last_error
+	bias = 0.35
+	kp = 0.9
+	ki = 0.18 / (10**9)
+
+	p_term = kp * error
+	i_term += ki * error * dt
+	d_term = 0
+
+	return p_term + i_term + d_term + bias
+
+
+def pid(dt,error):
+	global i_term,last_error
+	bias = 0.35
+	kp = 0.9
+	ki = 0.18 / (10**9)
+	kd = 0.0
+
+	p_term = kp * error
+	i_term += ki * error * dt
+	d_term = kd * (error-last_error) / dt
+
+	return p_term + i_term + d_term + bias
+
+
+# Create a list of the controller functions.
+controller_list = {
+	'p_func':p,
+	'pi_func':pi,
+	'pid_func':pid
+}
+
+
+def controller(pingpong_lab,v_target = 1.0,time_limit = 10):
+	global i_term,last_error
+
+	# Assign the controller function being used.
+	active_controller = controller_list.get(active_loop_func,p_func)
+
+	# Zero out memory terms for I and D.
+	i_term = 0
+	last_error = 0
+
+	# Break the pingpong ball off the bottom.
+	pingpong_lab.break_stall()
+
+	# Initiate times.
+	start_time = monotonic_ns()
+	last_time = start_time
+	end_time = start_time + (time_limit * 10**9)
+
+	# Run the control loop for the duration of the time limit.
+	while (last_time < end_time):
+
+		# Immediately update the time and sensor measurement.
+		now = monotonic_ns()
+		v_meas = pingpong_lab.sensor.volts
+
+		# Calculate error and dt.
+		error_val = v_target - v_meas
+		dt_val = max(now - last_time,1/10**9)	# use max() to avoid 0 Division errors. If dt=0, then set it to 1 ns.
+
+		# Poll the appropriate controller for duty cycle.
+		duty_cycle = active_controller(dt_val,error_val)
+
+		# Set the duty cycle.
+		pingpong_lab.set_pwm(duty_cycle)
+
+		# Print error and duty cycle to serial.
+		print(error_val,duty_cycle)
+
+		# Update memory aspects of I and D.
+		last_error = error_val
+		last_time = now
+
+		# Can run without this, but... it's already updating faster than the PWM Frequency
+		sleep(0.01)
+
+	# After the time limit has expired, set the pwm  duty cycle to 0.
+	pingpong_lab.set_pwm(0)
+
